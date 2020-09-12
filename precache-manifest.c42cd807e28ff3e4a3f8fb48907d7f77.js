b43b688d3a7183a69c169a9e06cd6b580586be2c
warlord9808 <umidghimire11@gmail.com>
2020-09-12 16:06:59 +0545

    Updates

---
+++ precache-manifest.c42cd807e28ff3e4a3f8fb48907d7f77.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a5bf9b6ec47050e15bd9df4d10a7db4",
+    "url": "/scanner/index.html"
+  },
+  {
+    "revision": "e343bdf60601e7c967c2",
+    "url": "/scanner/static/css/2.1bce3852.chunk.css"
+  },
+  {
+    "revision": "05c13c5b0b17f6532d27",
+    "url": "/scanner/static/css/main.af9ba318.chunk.css"
+  },
+  {
+    "revision": "e343bdf60601e7c967c2",
+    "url": "/scanner/static/js/2.80e093dc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/scanner/static/js/2.80e093dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05c13c5b0b17f6532d27",
+    "url": "/scanner/static/js/main.4c094d39.chunk.js"
+  },
+  {
+    "revision": "6b5c8dba74aa1d938d2f",
+    "url": "/scanner/static/js/runtime-main.b2a6e43d.js"
+  }
+]);
