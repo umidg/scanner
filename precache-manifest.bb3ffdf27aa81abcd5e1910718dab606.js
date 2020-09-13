91ffe42eaf0204c9775891a65344c5c8eb817e4d
warlord9808 <umidghimire11@gmail.com>
2020-09-13 13:18:01 +0545

    Updates

--- precache-manifest.bb3ffdf27aa81abcd5e1910718dab606.js
+++ precache-manifest.bb3ffdf27aa81abcd5e1910718dab606.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "80a7d072ee8da0bfeded3a93a7629423",
+    "revision": "f0f3605458991ac932f57840e8da19ae",
     "url": "/scanner/index.html"
   },
   {
-    "revision": "14f1ffe81fbb1fb212b6",
+    "revision": "aa72b54ae3d9fdea8c47",
     "url": "/scanner/static/css/2.1bce3852.chunk.css"
   },
   {
-    "revision": "2783255da220b5722744",
+    "revision": "c7de31e07c341dde8be1",
     "url": "/scanner/static/css/main.af9ba318.chunk.css"
   },
   {
-    "revision": "14f1ffe81fbb1fb212b6",
-    "url": "/scanner/static/js/2.d9b94c36.chunk.js"
+    "revision": "aa72b54ae3d9fdea8c47",
+    "url": "/scanner/static/js/2.5bb5602b.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/scanner/static/js/2.d9b94c36.chunk.js.LICENSE.txt"
+    "url": "/scanner/static/js/2.5bb5602b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "2783255da220b5722744",
-    "url": "/scanner/static/js/main.9fae84bc.chunk.js"
+    "revision": "c7de31e07c341dde8be1",
+    "url": "/scanner/static/js/main.1a55410f.chunk.js"
   },
   {
     "revision": "6b5c8dba74aa1d938d2f",
